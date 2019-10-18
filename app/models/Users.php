6498542cfcbcf5c09d12a477b577b0208cf8b797
minhtriet2104 <minhtriet2104.it@gmail.com>
2019-10-18 13:22:50 +0700

    add find and create method for user

--- app/models/Users.php
+++ app/models/Users.php
@@ -1,9 +1,25 @@
 <?php
 class Users extends Database
 {
+  // Get user list
     public function getUsersList()
     {
         $sql = parent::$connection->prepare('SELECT * FROM users');
         return parent::select($sql);
+    }
+
+    // find username
+    public function findUsername($username) {
+      $sql = parent::$connection->prepare('SELECT * FROM users WHERE user_name = ?');
+      $sql->bind_param('s', $username);
+      return parent::select($sql);
+    }
+  
+    // Creat new user
+    public function createUser($username, $password) {
+      $role = "member";
+      $sql = parent::$connection->prepare('INSERT INTO `users` (`user_id`, `user_name`, `user_password`, `user_role`) VALUES (NULL, ?, ?, ?)');
+      $sql->bind_param('sss', $username, $password, $role);
+      return $sql->execute();
     }
 }   
